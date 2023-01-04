277b4671fc86e5e5753012a31c1cc4304ba6270c
Rade58 <bajic.rade2@gmail.com>
2023-01-04 23:15:30 +0100

    🦉

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage as NP } from "next";
+import { useRouter } from "next/router";
+
+const BlogPostPage: NP = () => {
+  return <div>Blog Post </div>;
+};
+
+export default BlogPostPage;
--- app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage as NP } from "next";
-
-const BlogPage: NP = () => {
-  return <div>Blog</div>;
-};
-
-export default BlogPage;
