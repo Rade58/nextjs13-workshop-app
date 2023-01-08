015088b31ad6b33abd5c3e3cc8a2fbb5d187ca4d
Rade58 <bajic.rade2@gmail.com>
2023-01-08 14:43:05 +0100

    🦉

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,3 @@
-/* export const generateStaticParams = () => {
-  return [
-    { slug: "learn-types" },
-    { slug: "svelte-vs-react" },
-    { slug: "hello-world" },
-  ];
-}; */
-
 const BlogHomePage = () => {
   return <div>Blog home page</div>;
 };
